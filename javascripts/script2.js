c1ec86d5f2a479f6c77abbd95881debbc517fd8e
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-16 19:56:48 +0300

    script file saved separetaly

---
+++ javascripts/script2.js
@@ -0,0 +1,12 @@
+function alert_mygtukas() {
+				document.getElementById('demo').innerHTML = 'Blogai';
+			}
+			function date_mygtukas() {
+				document.getElementById('demo').innerHTML = Date();
+			}
+			function teisingai() {
+				document.getElementById('klausimas').innerHTML = 'Atsakymas teisingas';
+			}
+			function neteisingai() {
+				document.getElementById('klausimas').innerHTML = 'Atsakymas neteisingas';
+			}
